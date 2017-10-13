3c9e1dbde751d2d6b2aca8b51e3cc54f77265fe7
张景明 <Firya@Firya.local>
2017-10-13 17:55:11 +0800

    超出隐藏

--- src/components/card.vue
+++ src/components/card.vue
@@ -202,6 +202,11 @@ export default {
 .txt {
   font-size: 14px;
   height: 50px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 }
 
 .time {
